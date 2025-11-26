f9218ecc6f26c77e439b1f2bfbed1a4a664aa27f
Kamel Haj Salah <kamel-chedly.ben-haj-salah@etu.unice.fr>
2025-11-26 11:44:43 +0100

    Feat: Implement Lidar filter node and simulation setup for TurtleBot3

---
+++ install/my_lidar_filter/share/my_lidar_filter/launch/start_all.launch.py
@@ -0,0 +1,42 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # 1. On récupère le chemin vers le package de simulation officiel
+    turtlebot3_gazebo_pkg = get_package_share_directory('turtlebot3_gazebo')
+
+    # 2. On définit le lancement de la simulation (Gazebo)
+    # On inclut le fichier "turtlebot3_world.launch.py" qui existe déjà
+    gazebo_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo_pkg, 'launch', 'turtlebot3_world.launch.py')
+        )
+    )
+
+    # 3. On définit le lancement du nœud (le filtre Lidar)
+    # Assure-toi que 'package' est le nom de ton dossier et 'executable' celui défini dans setup.py
+    lidar_filter_node = Node(
+        package='my_lidar_filter',
+        executable='filter',
+        name='closest_point_filter',
+        output='screen'
+    )
+
+    # 4. On définit le lancement de RViz2 pour voir les résultats
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen'
+    )
+
+    # 5. On retourne la description complète : Gazebo + Ton Filtre + RViz
+    return LaunchDescription([
+        gazebo_sim,
+        lidar_filter_node,
+        rviz_node
+    ])
